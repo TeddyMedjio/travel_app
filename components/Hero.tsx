8fb41242233754b75b8be5567d2b6303016d5303
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-10-20 12:24:22 +0100

    update full property

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,12 +42,13 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button type="button" title="Download App" variant="btn_green" />
+          <Button type="button" title="Download App" variant="btn_green" full />
           <Button
             type="button"
             title="How we work?"
             icon="/play.svg"
             variant="btn_white_text"
+            full
           />
         </div>
       </div>
